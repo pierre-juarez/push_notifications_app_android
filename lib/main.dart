67f7ec2ea9c06b5b34d87b5b646c80634d6b4289
Pierre Juarez U. <76144339+pierre-juarez@users.noreply.github.com>
2025-05-04 01:10:01 -0500

    [feat] - Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:push_app/config/theme/app_theme.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(body: Center(child: Text('Hello World!'))),
+      theme: AppTheme().getTheme(),
+    );
+  }
+}
